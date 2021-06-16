3d09e11079ace03e1c5a8da38dd625052328d271
马静 <573732557@qq.com>
2021-06-16 21:43:15 +0800

    添加了一行代码

---
+++ src/Name.java
@@ -0,0 +1,12 @@
+import javax.sound.midi.Soundbank;
+
+public class Name {
+
+    public static void main(String[] args){
+
+        System.out.println("hello");
+        System.out.println("马静");
+        System.out.println("中国");
+
+    }
+}
